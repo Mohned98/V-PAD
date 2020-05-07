3d93196d0c95ffed66ddb88e17e9e2274e1cdca5
yasmine96 <yasminemohamedelsayed@gmail.com>
2020-05-07 03:13:55 +0200

    splitting cam window into 2 regions

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+
 from tkinter import *
-import keypad
 import tkinter.font as tkFont
 import tkinter.messagebox as tkMessageBox
+import subprocess
 
 
 root = Tk()
@@ -21,10 +22,10 @@ welcome.pack( pady=10,fill=X)
 
 
 def start_btn():
-    welcome.pack_forget()
-    Startbutton.pack_forget()
-    res=keypad()
-
+    root.wm_withdraw()
+    process = subprocess.Popen("python Window.py",
+                                    shell=True,
+                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 Startbutton = Button( root, text="Let's Start...", fg="blue", command=start_btn ,font = fontStyle)
@@ -35,4 +36,3 @@ Startbutton.place(x=250,y=170)
 
 
 root.mainloop()
-
